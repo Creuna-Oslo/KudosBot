760d92ae0901f63be84fefaab7c0d97535c620c2
Kristoffer Finckenhagen <k.finckenhagen@osl-krfi01x.co.int>
2018-09-19 16:56:10 +0200

    code refactoring

--- cudos_server/server.js
+++ cudos_server/server.js
@@ -8,9 +8,7 @@ const _cudoType = ["avocado", "unicorn_face", "tada"];
 const url = _security["azure"];
 const { SlackOAuthClient } = require("messaging-api-slack");
 
-const slackClient = SlackOAuthClient.connect(
-  _security["oauth"]
-);
+const slackClient = SlackOAuthClient.connect(_security["oauth"]);
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -98,190 +96,141 @@ app.get("/getTopFive", (req, res) => {
   );
 });
 
-app.post("/cudos", (req, res) => {
-  const payload = req.body;
-  const user = payload["user_name"];
-  mongoClient.connect(
-    url,
-    (err, db) => {
-      if (err) throw err;
-      var dbo = db.db("creuna-cudos");
-      dbo.collection("users").findOne({ 'id': user }, (err, result) => {
-            if (err) throw err;
-        var payload = { text: `cudos to give: ${result["cudos"]}\n cudos given: ${result["cudos_given"]}\n :avocado: ${result["avocado"]}\n :unicorn_face: ${result["unicorn_face"]}\n :tada: ${result["tada"]}` };
-            res.send(payload);
-            db.close();
-          }
-        );
-    } );
-});
-
 validToken = token => token == _security["token"];
 
-validateCudoBalance = userData => userData["cudos"] > 0;
+validCudoBalance = userData => userData["cudos"] > 0;
 
-getUserData = async user => {
+getData = async func => {
   let client = await mongoClient.connect(url);
   let db = client.db("creuna-cudos");
   try {
-    return await db.collection("users").findOne({ id: user });
+    return await func(db);
   } finally {
     client.close();
   }
 };
 
-updateCudos = async (userData, type, value) => {
-  let client = await mongoClient.connect(url);
-  let db = client.db("creuna-cudos");
-  try {
-    db.collection("users").updateOne(
-      { id: userData["id"] },
-      { $set: { [type]: value } }
-    );
-  } finally {
-    client.close();
-  }
-};
+getUserData = async user =>
+  await getData(async db => await db.collection("users").findOne({ id: user }));
+
+getTopListData = async type =>
+  await getData(
+    async db =>
+      await db
+        .collection("users")
+        .find({})
+        .sort({ [type]: -1 })
+        .limit(3)
+        .toArray()
+  );
+
+updateCudos = async (userData, type, value) =>
+  await getData(async db =>
+    db
+      .collection("users")
+      .updateOne({ id: userData["id"] }, { $set: { [type]: value } })
+  );
 
 giveCudos = async (fromUser, toUser, type, message) => {
-  if (_cudoType.indexOf(type) > -1) {
-    var fromUserData = await getUserData(fromUser);
-    var toUserData = await getUserData(toUser);
-    var newFromCudosValue = fromUserData["cudos"] - 1;
-    var newFromCudosGivenValue = fromUserData["cudos_given"] + 1;
-    var newToCudosReceivedValue = toUserData[type] + 1;
-    if (validateCudoBalance(fromUserData)) {
-      await updateCudos(fromUserData, "cudos", newFromCudosValue);
-      await updateCudos(fromUserData, "cudos_given", newFromCudosGivenValue);
-      await updateCudos(toUserData, type, newToCudosReceivedValue);
-      sendMessageToReceiver(fromUser, toUser, type, message);
-      return { text: `gave 1 cudo to ${toUser}` };
+  if (_cudoType.indexOf(type) <= -1)
+    return 'Invalid cudo type, use :avocado:, :unicorn_face: or :tada:';
+  var fromUserData = await getUserData(fromUser);
+  if (!validCudoBalance(fromUserData))
+    return "You don't have enough cudos to peform this action";
+  var toUserData = await getUserData(toUser);
+  await updateCudos(fromUserData, "cudos", fromUserData.cudos -1);
+  await updateCudos(fromUserData, "cudos_given", fromUserData.cudos_given + 1);
+  await updateCudos(toUserData, type, toUserData[type] + 1);
+  sendMessageToReceiver(fromUser, toUser, type, message);
+  return `gave 1 cudo to ${toUser}`;
+};
 
-    } else return { text: "You don't have enough cudos to peform this action" };
-  } else return { text: "Not a valid cudo type, use :avocado:, :unicorn_face: or :tada:" };
-}
-
-sendMessageToReceiver = async (fromUser, toUser, type, message ) => {
+sendMessageToReceiver = async (fromUser, toUser, type, message) => {
   var toUserID = await getToUserID(toUser);
-  slackClient.postMessage(toUserID, {
-    text: `${fromUser} har sendt deg en :${
-      type
-      }:, med melding _"${message}"_`
-  }, { as_user: true });
-}
+  slackClient.postMessage(
+    toUserID,
+    {
+      text: `${fromUser} har sendt deg en :${type}:, med melding _"${message}"_`
+    },
+    { as_user: true }
+  );
+};
 
-validText = (text, res) => {
-  if (text == '') {
-    res.send({ text: 'Please specify user, cudo type and message' });
-    return false;
-  }
-  var textSplit = text.split(" ");
-  if (textSplit.length < 3) {
-    res.send({ text: 'Please specify user, cudo type and message' });
-    return false;
-  }
-  return textSplit;
-}
-
-getToUserID = async (username) => {
+getToUserID = async username => {
   var users = await slackClient.getAllUserList();
-  var userID = '';
+  var userID = "";
   users.forEach(user => {
-    if (user['name'] == username) {
-      userID = user['id']
+    if (user["name"] == username) {
+      userID = user["id"];
     }
-  })
-  return userID;
-}
+  });
+  return !userID == "" ? userID : false;
+};
 
 const asyncMiddleware = fn => (req, res, next) => {
   Promise.resolve(fn(req, res, next)).catch(next);
 };
 
-app.post('/giveCudos', asyncMiddleware(async (req, res, next) => {
-  const payload = req.body;
-  const textSplit = validText(payload['text'], res);
-  if (!textSplit) {
-    console.log('Illegal input from slack user');
-    return;
+app.post(
+  "/cudos",
+  asyncMiddleware(async (req, res, next) => {
+    const userName = req.body["user_name"];
+    let user = await getUserData(userName);
+    var payload = {
+      text: `cudos to give: ${user.cudos}\n cudos given: ${
+        user.cudos_given
+      }\n :avocado: ${user.avocado}\n :unicorn_face: ${
+        user.unicorn_face
+      }\n :tada: ${user.tada}`
+    };
+    res.send(payload);
+  })
+);
+
+parseGiveCudosParameters = payload => {
+  var result = {};
+  var textSplit = payload.text.split(" ");
+  if (textSplit.length < 3) {
+    result.error = "Please specify user, cudo type and message";
+    return result;
   }
-  const fromUser = payload["user_name"];
-  const toUser = textSplit[0].substring(1);
-  const type = textSplit[1].slice(1, -1);
-  const message = textSplit.slice(2, textSplit.length).join(' ');
-  if(fromUser == toUser){
-    res.send({ text: "Jeez.. giving cudos to yourself? :exploding_head:" });
-    return;
-  }
-  const token = payload["token"]
-  var response = validToken(token) ?
-    await giveCudos(fromUser, toUser, type, message)
-    : { text: "Invalid token" };
-  res.send(response);
-}));
+  result.toUser = textSplit[0].substring(1);
+  result.fromUser = payload.user_name;
+  result.type = textSplit[1].slice(1, -1);
+  result.message = textSplit.slice(2, textSplit.length).join(" ");
+  result.token = payload.token;
+  if (!validToken(result.token)) result.error = "Invalid token";
+  else if (result.fromUser == result.toUser)
+    result.error = "Jeez.. giving cudos to yourself? :exploding_head:";
+  return result;
+};
+
+app.post(
+  "/giveCudos",
+  asyncMiddleware(async (req, res, next) => {
+    const parameters = parseGiveCudosParameters(req.body);
+    if (parameters.error) {
+      res.send(parameters.error);
+      return;
+    }
+    let result = await giveCudos(
+      parameters.fromUser,
+      parameters.toUser,
+      parameters.type,
+      parameters.message
+    );
+    res.send({text: result});
+  })
+);
 
 app.listen(app.get("port"), () => {
   console.log("Example app listening on port " + app.get("port"));
 });
 
-//for display
-app.get("/getTopAvocado", (req, res) => {
-  const user = req.query.id;
-  mongoClient.connect(
-    url,
-    (err, db) => {
-      if (err) throw err;
-      var dbo = db.db("creuna-cudos");
-      dbo
-        .collection("users")
-        .find({})
-        .sort({ avocado: -1 })
-        .limit(3)
-        .toArray((err, result) => {
-          if (err) throw err;
-          res.send(result);
-          db.close();
-        });
-    }
-  );
-});
-app.get("/getTopConfetti", (req, res) => {
-  const user = req.query.id;
-  mongoClient.connect(
-    url,
-    (err, db) => {
-      if (err) throw err;
-      var dbo = db.db("creuna-cudos");
-      dbo
-        .collection("users")
-        .find({})
-        .sort({ avocado: -1 })
-        .limit(3)
-        .toArray((err, result) => {
-          if (err) throw err;
-          res.send(result);
-          db.close();
-        });
-    }
-  );
-});
-app.get("/getTopUnicorn", (req, res) => {
-  const user = req.query.id;
-  mongoClient.connect(
-    url,
-    (err, db) => {
-      if (err) throw err;
-      var dbo = db.db("creuna-cudos");
-      dbo
-        .collection("users")
-        .find({})
-        .sort({ avocado: -1 })
-        .limit(3)
-        .toArray((err, result) => {
-          if (err) throw err;
-          res.send(result);
-          db.close();
-        });
-    }
-  );
-});
+app.get(
+  "/getTopList",
+  asyncMiddleware(async (req, res, next) => {
+    let result = await getTopListData(req.query.type);
+    res.send(result);
+  })
+);
